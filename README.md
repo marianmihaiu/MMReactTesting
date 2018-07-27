221009b210b9b923d2b2bd35aa6739cfbbff79d3
mmihaiu67 <31453816+mmihaiu67@users.noreply.github.com>
2018-07-27 16:00:56 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# MMReactTesting
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const headerStyle = {
+      color: "#ff0000",
+      textDecoration: "underline"
+    };
+    return (
+      <div>
+        <h2 style={headerStyle}>Hello World Again!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
